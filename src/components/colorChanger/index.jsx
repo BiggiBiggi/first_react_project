d0f1e2931e37a796e30352f75907a09bffdc26cc
BiggiBiggi <frelandbenjamin99@gmail.com>
2024-07-09 10:51:58 +0200

    Création dossier Component 'ColorChange'

---
+++ src/components/colorChanger/index.jsx
@@ -0,0 +1,50 @@
+const ColorChanger = ({ name }) => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+  const divStyle = {
+    width: "100px",
+    height: "100px",
+    backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+  };
+  const handleIncrement = (colorSetter, colorValue) => {
+    colorSetter(Math.min(colorValue + 6.66, 255));
+  };
+  const handleDecrement = (colorSetter, colorValue) => {
+    colorSetter(Math.max(colorValue - 6.66, 0));
+  };
+  return (
+    <div>
+      <div style={divStyle}></div>
+      <div>
+        <h3>Red: {red}</h3>
+        <button onClick={() => handleIncrement(setRed, red)}>
+          Increment Red
+        </button>
+        <button onClick={() => handleDecrement(setRed, red)}>
+          Decrement Red
+        </button>
+      </div>
+      <div>
+        <h3>Green: {green}</h3>
+        <button onClick={() => handleIncrement(setGreen, green)}>
+          Increment Green
+        </button>
+        <button onClick={() => handleDecrement(setGreen, green)}>
+          Decrement Green
+        </button>
+      </div>
+      <div>
+        <h3>Blue: {blue}</h3>
+        <button onClick={() => handleIncrement(setBlue, blue)}>
+          Increment Blue
+        </button>
+        <button onClick={() => handleDecrement(setBlue, blue)}>
+          Decrement Blue
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ColorChanger;
